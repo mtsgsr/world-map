e602f2724971923ef40d42395033b0b616235aa5
Matheus Gomes <maatheus.gsr@gmail.com>
2023-03-08 19:13:17 -0300

    Add new query getPageBySlug and first param to getPages

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,12 +1,25 @@
 import { gql } from "graphql-request";
 
 export const GET_PAGES = gql`
-  query getPages {
-    pages {
+  query getPages($first: Int) {
+    pages(first: $first) {
       id
       heading
       slug
       body {
+        html
+      }
+    }
+  }
+`;
+
+export const GET_PAGE_BY_SLUG = `
+  query getPageBySlug($slug: String){
+    page(where: {slug: $slug}){
+      id
+      slug
+      heading
+      body{
         html
       }
     }
