2ace2e095848215a821ef51d02e17e8f814b777b
Matheus Gomes <maatheus.gsr@gmail.com>
2023-03-09 08:55:00 -0300

    Add place styles and next image config

--- src/templates/Places/index.tsx
+++ src/templates/Places/index.tsx
@@ -1,3 +1,10 @@
+import LinkWrapper from "@/components/LinkWrapper";
+import { CloseOutline } from "@styled-icons/evaicons-outline";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+import * as S from "./styles";
+
 type ImageProps = {
   url: string;
   height: number;
@@ -18,17 +25,31 @@ export type PlaceTemplateProps = {
 export default function PlaceTemplate({ place }: PlaceTemplateProps) {
   return (
     <>
-      <h1>{place.name}</h1>
-      <div dangerouslySetInnerHTML={{ __html: place.description.html }} />
-      {place.gallery.map((image, index) => {
-        <img
-          key={`photo-${index}`}
-          src={image.url}
-          alt={place.name}
-          width={image.width}
-          height={image.height}
-        />;
-      })}
+      <LinkWrapper href="/">
+        <CloseOutline size={32} aria-label="Voltar para o mapa" />
+      </LinkWrapper>
+
+      <S.Wrapper>
+        <S.Container>
+          <S.Heading>{place.name}</S.Heading>
+          <S.Body
+            dangerouslySetInnerHTML={{ __html: place.description?.html || "" }}
+          />
+          <S.Gallery>
+            {place.gallery.map((image, index) => (
+              <Image
+                key={`photo-${index}`}
+                src={image.url}
+                alt={place.name}
+                width={1000}
+                height={600}
+                quality={75}
+                draggable="false"
+              />
+            ))}
+          </S.Gallery>
+        </S.Container>
+      </S.Wrapper>
     </>
   );
 }
