8e338d100a7199abd9797c8abf7d0b3323ef0aae
Matheus Gomes <maatheus.gsr@gmail.com>
2023-03-23 12:04:43 -0300

    Fix crossorigin attribute

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,7 +15,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           rel="stylesheet"
           href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
           integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
-          crossorigin=""
+          crossOrigin=""
         />
       </Head>
       <DefaultSeo
